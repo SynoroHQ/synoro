479fdcf4541429181302f5b154ca46cc8fd0ff3b
Maksim <kodermax@gmail.com>
2025-08-26 16:15:58 +0300

    up

--- packages/emails/index.ts
+++ packages/emails/index.ts
@@ -1,9 +1,3 @@
-export { default as EmailChangeVerification } from "./emails/auth/email-change-verification";
-export { default as EmailVerification } from "./emails/auth/email-verification";
-export { default as OtpSignInEmail } from "./emails/auth/otp-sign-in";
-export { default as PasswordResetEmail } from "./emails/auth/password-reset";
-export { default as WelcomeEmail } from "./emails/welcome";
-
 // Экспортируем утилиты для перевода
 export * from "./src/utils";
 
