542a6e2a3cb9d7b05b48cb18875f0cffc49eedf6
Maksim <kodermax@gmail.com>
2025-09-07 13:22:45 +0300

    up

--- packages/db/src/schemas/reminders/index.ts
+++ packages/db/src/schemas/reminders/index.ts
@@ -1,47 +1,8 @@
 // Export schema
 export * from "./schema";
 
-// Export types from validators (only non-conflicting ones)
-export type {
-  ReminderType,
-  ReminderPriority,
-  ReminderStatus,
-  ReminderRecurrence,
-  ExecutionStatus,
-  NotificationChannel,
-  Reminder,
-  ReminderUpdate,
-  ReminderExecution,
-  ReminderExecutionUpdate,
-  ReminderTemplate,
-  ReminderTemplateUpdate,
-  ReminderFilters,
-  ReminderSortOptions,
-  ReminderSearch,
-  CreateFromTextRequest,
-  ReminderIdRequest,
-  CompleteReminderRequest,
-  SnoozeReminderRequest,
-  GetReminderRequest,
-  FindSimilarRequest,
-  BulkReminderOperation,
-} from "@synoro/validators";
-
 // Export utilities
 export * from "./utils";
 
 // Export constants
 export * from "./constants";
-
-// Export validation functions from validators
-export {
-  validateReminder,
-  validateReminderUpdate,
-  validateReminderExecution,
-  validateReminderTemplate,
-  validateReminderSearch,
-  validateBulkOperation,
-  validateReminderTime,
-  validateRecurrencePattern,
-  validateTags,
-} from "@synoro/validators";
